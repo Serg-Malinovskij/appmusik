b10937fc80de7f3ec4536413248346bebbcd8759
Малиновский Сергей <104252025+Serg-Malinovskij@users.noreply.github.com>
2024-02-12 23:25:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,1 @@
-// scripts.js
-function vieport() {
-  window.addEventListener('resize', function () {
-      set_100vh_var();
-      detect_mobile();
-  });
-  window.addEventListener('orientationchange', function () {
-      set_100vh_var();
-  });
-  
-  setTimeout(
-      set_100vh_var, 
-      50
-  );
-}
 
-function set_100vh_var() {
-  document.documentElement.style.setProperty('--vh', `${window.innerHeight/100}px`);
-};
